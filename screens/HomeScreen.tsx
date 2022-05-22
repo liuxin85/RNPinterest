4991960c28b0aca31b320fb8e6a78f19609e14ef
Xin Liu <liuxin85@gmail.com>
2022-05-22 13:21:14 +0800

    profile screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,42 +1,6 @@
-import { StyleSheet, FlatList, Image, ScrollView } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
-import Pin from "../components/Pin";
-import { Text, View } from "../components/Themed";
-import { RootTabScreenProps } from "../types";
 import pins from "../assets/data/pins";
+import MasonryList from "../components/MasonryList";
 
-export default function HomeScreen({
-  navigation,
-}: RootTabScreenProps<"TabOne">) {
-  return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 0)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-        <View style={styles.column}>
-          {pins
-            .filter((_, index) => index % 2 === 1)
-            .map((pin) => (
-              <Pin pin={pin} key={pin.id} />
-            ))}
-        </View>
-      </View>
-    </ScrollView>
-  );
+export default function HomeScreen() {
+  return <MasonryList pins={pins} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-  },
-  column: {
-    flex: 1,
-  },
-});
